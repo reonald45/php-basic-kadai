3a26807fa1a700eba600756f0dde1f8e183e08ac
reonald45 <151507654+reonald45@users.noreply.github.com>
2024-02-08 09:49:24 +0900

    7章課題の追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <meta charset="UTF-8">
+  <title>PHP7章課題</title>
+
+  <body>
+    <p>
+      <?php
+      $tamanegi = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($tamanegi);
+      ?>
+    </p>
+  </body>
+
+</html>
